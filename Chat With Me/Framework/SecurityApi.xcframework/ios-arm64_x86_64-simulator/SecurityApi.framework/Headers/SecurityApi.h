2e95e95517fa63d96ab1a99bb20b6de6d506db8a
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-02-24 15:49:30 -0300

    Framework - Import Security sdk to handle with keys - Thiago Batista

---
+++ Chat With Me/Framework/SecurityApi.xcframework/ios-arm64_x86_64-simulator/SecurityApi.framework/Headers/SecurityApi.h
@@ -0,0 +1,18 @@
+//
+//  SecurityApi.h
+//  SecurityApi
+//
+//  Created by Beltrami Dias Batista, Thiago on 24/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecurityApi.
+FOUNDATION_EXPORT double SecurityApiVersionNumber;
+
+//! Project version string for SecurityApi.
+FOUNDATION_EXPORT const unsigned char SecurityApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecurityApi/PublicHeader.h>
+
+
