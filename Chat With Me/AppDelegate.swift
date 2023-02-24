1831a43ff2ddfa3848974ea097dbece2447df813
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-02-24 15:44:01 -0300

    Framework - Import Security sdk to handle with keys - Thiago Batista

--- Chat With Me/AppDelegate.swift
+++ Chat With Me/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import IQKeyboardManagerSwift
+import SecurityApi
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,13 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        let db =  Firestore.firestore()
-        print(db)
+        
         
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+
+        SecurityApi.SecurityPlist().savePlistValue()
+    
+        FirebaseApp.configure()
+        _ =  Firestore.firestore()
         
         return true
     }
@@ -39,7 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
